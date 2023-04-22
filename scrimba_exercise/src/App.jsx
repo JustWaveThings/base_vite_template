eaa9d73ef1835ccf0e5d1c8e5fafc755c6db4259
JustWaveThings <justwavethings@proton.me>
2023-04-21 22:46:51 -0400

    base template repo

--- scrimba_exercise/src/App.jsx
+++ scrimba_exercise/src/App.jsx
@@ -1,23 +1,7 @@
-import Main from './components/Main';
-import Navbar from './components/Navbar';
 import React from 'react';
 
 function App() {
-	const [darkMode, setDarkMode] = React.useState(false);
-
-	function handleDarkMode() {
-		setDarkMode(prevState => !prevState);
-	}
-
-	return (
-		<div className="container">
-			<Navbar
-				toggleDarkMode={handleDarkMode}
-				darkMode={darkMode}
-			/>
-			<Main darkMode={darkMode} />
-		</div>
-	);
+	return <div></div>;
 }
 
 export default App;
